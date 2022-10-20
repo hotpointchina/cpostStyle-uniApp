ca365a5d9fa51c0dda168277b9875559b2be9ece
Azir <41815835+hotpointchina@users.noreply.github.com>
2022-10-20 08:23:59 +0800

    Add files via upload

---
+++ gongyiMobile/pages/index/index.vue
@@ -0,0 +1,427 @@
+<template>
+	<view class="gy_container">
+		<view class="key_show">
+			<view class="gy_info">
+				<view class="box">
+					<view class="line">
+						<text class="word">爱心公益活动</text>
+						<img class="tag_img" src="/static/tempPic/index/tag.png" />
+					</view>
+					
+					<view class="line">
+						<text class="summary">邮政公益，邮联万家</text>
+					</view>
+				</view>
+			</view>
+			
+			<view class="user_click" @click="userInfoPage">
+				<img class="user_img" src="/static/tempPic/index/user.png" />
+			</view>
+			
+			<img class="key_show_img" src="/static/tempPic/index/dog.jpg" />
+		</view>
+		
+		<view class="container_wrap_box" :style="{'padding-bottom':isSafari?'150px':'60px'}">
+			<!-- 捐赠类型 -->
+			<view class="box">
+				<view class="category_title_botton">
+					<text class="word">捐赠类型</text>
+					<view class="gy_button">网点查询</view>
+				</view>
+				<view class="jz_type">
+					<view class="info">
+						<text class="title">衣服</text>
+						<text class="summary">为世界添加一分温暖</text>
+						<!-- <button type="warn">我要捐赠</button> -->
+						<view class="gy_button">我要捐赠</view>
+					</view>
+					<view class="pic">
+						<img mode="widthFix" class="pic_img" src="/static/tempPic/index/clothes.jpg" />
+					</view>
+				</view>
+			</view>
+			
+			<!-- 捐赠流程 -->
+			<view class="box">
+				<text class="category_title">捐赠流程</text>
+				<view class="jz_sop">
+					<view class="crew">
+						<view class="pic">
+							<img class="file_img" src="/static/tempPic/index/file.png" />
+							<uni-icons type="forward" size="20" class="sop_icon"></uni-icons>
+						</view>
+						<text class="word">捐赠下单</text>
+					</view>
+					
+					<view class="crew">
+						<view class="pic">
+							<img class="you_ring_img" src="/static/tempPic/index/you-ring.png" />
+							<uni-icons type="forward" size="20" class="sop_icon"></uni-icons>
+						</view>
+						<text class="word">网点自寄</text>
+					</view>
+					
+					<view class="crew">
+						<view class="pic">
+							<img class="box_img" src="/static/tempPic/index/box.png" />
+							<uni-icons type="forward" size="20" class="sop_icon"></uni-icons>
+						</view>
+						<text class="word">机构签收</text>
+					</view>
+					
+					<view class="crew">
+						<view class="pic">
+							<img class="heart_img" src="/static/tempPic/index/taoxin.png" />
+							<uni-icons type="forward" size="20" class="sop_icon"></uni-icons>
+						</view>
+						<text class="word">公益执行</text>
+					</view>
+				</view>
+			</view>
+			
+			<!-- 旧衣回收去哪里了？ -->
+			<view class="box">
+				<text class="category_title">旧衣回收去哪里了？</text>
+				<view class="clothes_where">
+					<view class="box">
+						<img class="pic" src="/static/tempPic/index/city.jpg" />
+						<view class="word_box">
+							<text class="word">环保再生</text>
+							<view class="arrow"></view>
+						</view>
+					</view>
+					
+					<view class="box">
+						<img class="pic" src="/static/tempPic/index/cat.jpg" />
+						<view class="word_box">
+							<text class="word">旧衣出口</text>
+							<view class="arrow"></view>
+						</view>
+					</view>
+					
+					<view class="box">
+						<img class="pic" src="/static/tempPic/index/book.jpg" />
+						<view class="word_box">
+							<text class="word">环保再造</text>
+							<view class="arrow"></view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script setup>
+	import { reactive, ref } from "vue";
+	const data = reactive({
+		title: 'VUE3-uni-app-test'
+	});
+	
+	const userInfoPage = ()=>{
+		console.log( 'userInfoPage' );
+	};
+	
+	const isSafari = ref(false);
+	// #ifdef H5
+	const userAgent = ref('');
+	userAgent.value = navigator.userAgent.toLocaleLowerCase();
+	if( userAgent.value.indexOf('safari') ){
+		isSafari.value = true;
+	}
+	// #endif
+</script>
+
+<style lang="scss" scoped>
+	@mixin row{
+		display: flex;
+		flex-flow: row nowrap;
+	}
+	@mixin column {
+		display: flex;
+		flex-direction: column;
+	}
+	
+	.gy_container {
+		height: calc(100vh - 44px);
+		background-color: #fff;
+		/*  #ifdef  H5  */
+		position: fixed;
+		top: 44px;
+		left: 0px;
+		z-index: 9;
+		width: 100vh;
+		overflow-y: scroll;
+		box-sizing: content-box;
+		/* #endif */
+		font-size: 14px;
+		line-height: 24px;
+	}
+	
+	
+	// 顶部图
+	.key_show{
+		@include row;
+		justify-content: flex-start;
+		align-items: flex-start;
+		position: relative;
+		width: 750rpx;
+		height: 280rpx;
+		overflow: hidden;
+	}
+	.key_show_img{
+		width: 100%;
+		opacity: .5;
+	}
+	.gy_info{
+		position: absolute;
+		top:50rpx;
+		left:90rpx;
+		z-index: 1;
+		
+		.box{
+			@include column;
+			justify-content: flex-start;
+			align-items: flex-start;
+		}
+		.line{
+			@include row;
+			justify-content: flex-start;
+			align-items: center;
+		}
+		
+		& .word{
+			font-size: 36rpx;
+		}
+		& .summary{
+			margin-top: 30rpx;
+			font-size: 26rpx;
+		}
+	}
+	.tag_img{
+		width: 34rpx;
+		height: 45rpx;
+		margin: -30rpx 0 0 17rpx;
+		opacity: 1;
+	}
+	
+	.user_click{
+		@include row;
+		justify-content: center;
+		align-items: center;
+		position: absolute;
+		top: 21rpx;
+		right: 32rpx;
+		z-index: 1;
+		width: 46rpx;
+		height: 50rpx;
+		
+		& .user_img{
+			width: 46rpx;
+			height: 46rpx;
+			opacity: 1;
+		}
+	}
+	
+	// 内容区
+	.container_wrap_box{
+		@include column;
+		justify-content: flex-start;
+		align-items: flex-start;
+		width: 750rpx;
+		padding-bottom: 60px;
+		/*  #ifdef  H5  */
+		box-sizing: content-box;
+		/* #endif */ 
+		
+		& .box{
+			@include column;
+			justify-content: flex-start;
+			align-items: flex-start;
+			width: 750rpx;
+			padding: 22rpx;
+			
+			& .category_title_botton{
+				@include row;
+				justify-content: space-between;
+				align-items: center;
+				width: 697rpx;
+				padding: 0rpx 0 29rpx 9rpx;
+				
+				& .word{
+					padding: 0 0 0 9rpx;
+					font-size: 26rpx;
+				}
+				& .gy_button{
+					width: 160rpx;
+					height: 60rpx;
+					padding: 0rpx 24rpx;
+					background-color: #18ae66;
+					border-radius: 6rpx;
+					color: #fff;
+					font-size: 27rpx;
+					line-height: 60rpx;
+					text-align: center;
+					box-sizing: border-box;
+				}
+			}
+			& .category_title{
+				padding: 20rpx 0 29rpx 9rpx;
+				font-size: 26rpx;
+			}
+		}
+	}
+	.jz_type{
+		@include row;
+		justify-content: flex-start;
+		align-items: flex-start;
+		width: 706rpx;
+		padding: 14rpx 10rpx;
+		background: linear-gradient(90deg,#e3e8f1,#dde2f1);
+		box-sizing: border-box;
+		
+		& .info{
+			@include column;
+			justify-content: space-between;
+			align-items: center;
+			width: 266rpx;
+			& .title{
+				 @include row;
+				 justify-content: center;
+				 align-items: center;
+				 padding: 29rpx 0 20rpx 0;
+				 color: #f20c0c;
+				 font-size: 37rpx;
+			}
+			& .summary{
+				padding-bottom: 20rpx;
+				color: #102867;
+				font-size: 24rpx;
+			}
+			& .gy_button{
+				width: 160rpx;
+				height: 60rpx;
+				padding: 0rpx 24rpx;
+				background-color: #f20c0c;
+				border-radius: 6rpx;
+				color: #fff;
+				font-size: 27rpx;
+				line-height: 60rpx;
+				text-align: center;
+				box-sizing: border-box;
+			}
+		}
+		& .pic{
+			@include row;
+			justify-content: center;
+			align-items: center;
+			width: 420rpx;
+			height: 240rpx;
+			overflow: hidden;
+			& .pic_img{
+				width: 400rpx;
+			}
+		}
+	}
+	
+	.jz_sop{
+		@include row;
+		justify-content: center;
+		align-items: center;
+		width: 706rpx;
+		padding: 76rpx 0 59rpx 0;
+		background: rgba(187, 187, 187, 0.17) none;
+		border: 2px solid #bbbbbb;
+		border-radius: 6px;
+		box-sizing: border-box;
+		
+		& .crew{
+			@include column;
+			justify-content: center;
+			align-items: center;
+			margin-right: 54rpx;
+			
+			& .pic{
+				@include row;
+				justify-content: center;
+				align-items: center;
+				position: relative;
+				height: 44rpx;
+				margin-bottom: 40rpx;
+				
+				& .sop_icon{
+					position: absolute;
+					top: 2rpx;
+					right: -70rpx;
+					z-index: 1;
+				}
+				& .file_img{
+					width: 33rpx;
+					height: 43rpx;
+				}
+				& .you_ring_img{
+					width: 44rpx;
+					height: 44rpx;
+				}
+				& .box_img{
+					width: 44rpx;
+					height: 33rpx;
+				}
+				& .heart_img{
+					width: 44rpx;
+					height: 39rpx;
+				}
+			}
+			& .word{
+				font-size: 22rpx;
+			}
+		}
+	}
+	
+	.clothes_where{
+		@include row;
+		justify-content: space-around;
+		align-items: flex-start;
+		width: 706rpx;
+		background: #fff none;
+		box-sizing: border-box;
+		
+		& .box{
+			@include column;
+			position: relative;
+			width: 226rpx;
+			height: 176rpx;
+			padding: 0;
+			
+			& .pic{
+				width: 226rpx;
+				height: 176rpx;
+				opacity: .7;
+			}
+			& .word_box{
+				@include row;
+				justify-content: center;
+				align-items: center;
+				position: absolute;
+				top: 56rpx;
+				left: 0;
+				z-index: 1;
+				width: 100%;
+				
+				& .word{
+					color: #fff;
+					font-size: 26rpx;
+				}
+				& .arrow{
+					width: 0;
+					height: 0;
+					margin-left: 11rpx;
+					border-left: 10rpx solid #fff;
+					border-top: 10rpx solid transparent;
+					border-bottom: 10rpx solid transparent;
+				}
+			}
+		}
+	}
+</style>
---
+++ gongyiMobile/pages/uni-ui/uni_ui_test.vue
@@ -0,0 +1,556 @@
+<template>
+	<view class="container">
+		
+		<!-- Badge 数字角标 -->
+		<uni-section title=" Badge 数字角标 " type="line" padding>
+			<view class="example-body">
+				<uni-badge class="uni-badge-left-margin" text="1" />
+				<uni-badge class="uni-badge-left-margin" text="2" type="primary" />
+				<uni-badge class="uni-badge-left-margin" text="34" type="success" />
+				<uni-badge class="uni-badge-left-margin" text="45" type="warning" />
+				<uni-badge class="uni-badge-left-margin" text="123" type="info" />
+			</view>
+		</uni-section>
+		
+		<view class="gy-divider"></view>
+		
+		<!-- calendar 日历 -->
+		<uni-section title=" calendar 日历 " type="line" padding>
+			<view class="calendar-content">
+				<uni-calendar class="uni-calendar--hook" :selected="calendarInfo.selected" :showMonth="false" @change="calendarChange" @monthSwitch="monthSwitch" />
+			</view>
+		</uni-section>
+		
+		<view class="gy-divider"></view>
+		
+		<!-- progress 进度 -->	
+		<uni-section title=" progress 进度 " type="line" padding>
+			<view class="uni-padding-wrap uni-common-mt">
+				<view class="progress-box" v-for="prList in progessList" :key="prList">
+					<progress class="progress_class" :percent="prList" show-info stroke-width="3" active />
+				</view>
+				<view class="progress-control">
+					<button type="primary" @click="setProgress">设置进度</button>
+					<button type="warn" @click="clearProgress">清除进度</button>
+				</view>
+			</view>
+		</uni-section>
+		
+		<view class="gy-divider"></view>
+		
+		<!-- button 按钮 -->
+		<uni-section title=" button 按钮 " type="line" padding>
+			<view class="columnbox">
+				<button type="primary">页面主操作 Normal</button>
+				<button type="primary" loading="true">页面主操作 Loading</button>
+				<button type="primary" disabled="true">页面主操作 Disabled</button>
+				<button type="default">页面次要操作 Normal</button>
+				<button type="default" disabled="true">页面次要操作 Disabled</button>
+				<button type="warn">警告类操作 Normal</button>
+				<button type="warn" disabled="true">警告类操作 Disabled</button>
+					
+				<button type="primary" plain="true">按钮</button>
+				<button type="primary" disabled="true" plain="true">不可点击的按钮</button>
+				<button type="default" plain="true">按钮</button>
+				<button type="default" disabled="true" plain="true">按钮</button>
+				<button class="mini-btn" type="primary" size="mini">按钮</button>
+				<button class="mini-btn" type="default" size="mini">按钮</button>
+				<button class="mini-btn" type="warn" size="mini">按钮</button>
+			</view>
+		</uni-section>
+		
+		
+		<!-- checkbox-group 多项 -->
+		<view class="gy-divider"></view>
+		<uni-section title=" checkbox-group 多项选择器 " type="line" padding>
+			<view>
+				<checkbox-group>
+					<label>
+						<checkbox value="cb" checked="true" />选中
+					</label>
+					<label>
+						<checkbox value="cb" />未选中
+					</label>
+				</checkbox-group>
+			</view>
+		</uni-section>
+		
+		
+		<!-- radio 单选 -->
+		<view class="gy-divider"></view>
+		<uni-section title=" radio 单选 " type="line" padding>
+			<radio-group class="uni-list" @change="radioChange">
+				<label class="rowbox" v-for="(item,index) in radioItems" :key="index">
+					<radio :id="item.name" :value="item.name" :checked="item.checked" :color="'#18AE66'" ></radio>
+					<label class="label-2-text" :for="item.name">
+						<text>{{item.value}}</text>
+					</label>
+				</label>
+			</radio-group>
+			
+			<view class="rowbox mt-50">
+				<view class="text">单选选中：{{data.radio1}}</view>
+				<uni-data-checkbox v-model="data.radio1" :localdata="data.sex"></uni-data-checkbox>
+			</view>
+			
+			<view class="rowbox mt-50">
+				<view class="text">多选选中：{{data.checkbox1}}</view>
+				<uni-data-checkbox multiple v-model="data.checkbox1" :localdata="data.hobby"></uni-data-checkbox>
+			</view>
+			<view class="rowbox mt-50">
+				<view class="text">单选选中：{{data.radio2}}</view>
+				<uni-data-checkbox mode="button" v-model="data.radio2" :localdata="data.sex"></uni-data-checkbox>
+			</view>
+			<view class="rowbox mt-50">
+				<view class="text">多选选中：{{data.checkbox2}}</view>
+				<uni-data-checkbox mode="button" multiple v-model="data.checkbox2" :localdata="data.hobby"></uni-data-checkbox>
+			</view>
+			
+		</uni-section>
+		
+		
+		<!-- slider 滑动选择器 -->
+		<view class="gy-divider"></view>
+		<uni-section title=" slider 滑动选择器 " type="line" padding>
+			<slider value="60"  step="5" />
+		</uni-section>
+			
+		<!-- switch 开关选择器 -->
+		<view class="gy-divider"></view>
+		<uni-section title=" switch 开关选择器 " type="line" padding>
+			<switch checked :color="'#18AE66'" />
+		</uni-section>
+			
+		
+		<!-- countdown 倒计时组件 -->
+		<view class="gy-divider"></view>
+		<uni-section title=" countdown 倒计时组件 " type="line" padding>
+			<uni-countdown :font-size="30" :day="1" :hour="2" :minute="30" :second="0" color="#FFFFFF" background-color="#18AE66" />
+		</uni-section>
+		
+		
+		<!-- data-picker 单列、和多列级联选择 -->
+		<view class="gy-divider"></view>
+		<uni-section title=" data-picker 单列、和多列级联选择 " type="line" padding>
+			<uni-data-picker :localdata="data.dataPickerData" popup-title="请选择班级" @change="dataPickerDataOnchange" @nodeclick="dataPickerDataOnnodeclick"></uni-data-picker>
+		</uni-section>
+		
+		
+		<!-- input -->
+		<view class="gy-divider"></view>
+		<uni-section title=" input " type="line" padding>
+			<input class="uni-input" confirm-type="search" placeholder="键盘右下角按钮显示为搜索" />
+		</uni-section>
+		
+		<!-- easyinput 组件是对原生input组件的增强 ，是专门为配合表单组件uni-forms (opens new window)而设计的，easyinput 内置了边框，图标等，同时包含 input 所有功能 -->
+		<view class="gy-divider"></view>
+		<uni-section title=" easyinput 组件是对原生input组件的增强 " type="line" padding>
+			<uni-easyinput v-model="inputValue" placeholder="请输入内容"></uni-easyinput>
+		</uni-section>
+		
+		
+		<!-- fab 悬浮按钮 -->
+		<uni-fab 
+			:pattern="data.fabPattern" 
+			:content="data.fabContent" 
+			:horizontal="'right'" 
+			:vertical="'bottom'"
+			:direction="'horizontal'" 
+			@trigger="fabTrigger" 
+			@fabClick="fabClick" 
+		/>
+		
+		
+		<!-- uni-fav 收藏按钮 -->
+		<view class="gy-divider"></view>
+		<uni-section title=" easyinput 组件是对原生input组件的增强 " type="line" padding>
+			<view class="rowbox">
+				<uni-fav :checked="data.favData.checkList[0]" class="favBtn" bg-color-checked="#18AE66" @click="favClick(0)" />
+				<uni-fav :checked="data.favData.checkList[1]" :star="false" class="favBtn" bg-color-checked="#18AE66" @click="favClick(1)" />
+				<uni-fav :checked="data.favData.checkList[2]" class="favBtn" :circle="true" bg-color="#dd524d"
+					bg-color-checked="#18AE66" fg-color="#ffffff" fg-color-checked="#ffffff" @click="favClick(2)" />
+				<uni-fav :checked="data.favData.checkList[3]" class="favBtn" bg-color="#f8f8f8" bg-color-checked="#eeeeee"
+					fg-color="#333333" fg-color-checked="#333333" @click="favClick(3)" />
+			</view>
+		</uni-section>
+		
+		
+		<!-- uni-indexed-list 索引列表 -->
+		<uni-section title=" uni-indexed-list 索引列表 " type="line" padding>
+			<view class="columnbox">
+				<button type="primary" @click="gotoIndexList">查看 uni-indexed-list 索引列表</button>
+			</view>
+		</uni-section>
+		
+		
+		<!-- link 链接 -->
+		<uni-section title=" link 链接 " type="line" padding>
+			<uni-link href="https://uniapp.dcloud.io/" text="https://uniapp.dcloud.io/" color="#18AE66"></uni-link>
+		</uni-section>
+		
+		
+		<!-- uni-pagination 分页器 -->
+		<uni-section title=" uni-pagination 分页器 " type="line" padding>
+			<uni-pagination :total="50" />
+		</uni-section>
+		
+		
+		<!-- uni-segmented-control 分段器 -->
+		<uni-section title="实心标签" type="line">
+			<view class="uni-padding-wrap uni-common-mt">
+				<uni-segmented-control :current="data.segmentedData.current" :values="data.segmentedData.items" :style-type="'text'"
+					:active-color="'#18AE66'" @clickItem="segmentedDataOnClickItem" />
+			</view>
+			<view class="content">
+				<view v-if="data.segmentedData.current === 0"><text class="content-text">选项卡1的内容</text></view>
+				<view v-if="data.segmentedData.current === 1"><text class="content-text">选项卡2的内容</text></view>
+				<view v-if="data.segmentedData.current === 2"><text class="content-text">选项卡3的内容</text></view>
+			</view>
+		</uni-section>
+		
+		
+		<!-- uni-steps 步骤条 -->
+		<uni-section title=" uni-steps 步骤条 " type="line" padding>
+			<uni-section title="基本用法" type="line" padding>
+				<uni-steps :options="data.stepsData.list1" active-color="#18AE66" :active="data.stepsData.active" />
+			</uni-section>
+			<uni-section title="自定义图标" type="line" padding>
+				<uni-steps :options="data.stepsData.list1" active-color="#18AE66" active-icon="checkbox" :active="data.stepsData.active" />
+			</uni-section>
+			<uni-section title="自定义图标" type="line" padding>
+				<uni-steps :options="data.stepsData.list1" active-color="#18AE66" active-icon="medal" :active="data.stepsData.active" />
+			</uni-section>
+			<uni-section title="纵向排列" type="line" padding>
+				<uni-steps :options="data.stepsData.list2" active-color="#18AE66" :active="data.stepsData.active" direction="column" />
+			</uni-section>
+		</uni-section>
+		
+		
+		<!-- uni-table 表格 -->
+		<uni-section title=" uni-table 表格 " type="line" padding>
+			<button type="primary" @click="gotoTable">查看 Table</button>
+		</uni-section>
+		
+		
+		
+		
+	</view>
+</template>
+
+<script setup>
+import { reactive, ref, toRefs } from "vue";
+
+	const data = reactive({
+		calendarInfo:{
+			lunar: true,
+			range: true,
+			insert: false,
+			selected: []
+		},
+		progessList: [0, 0, 0, 0],
+		radioItems: [
+			{
+				name: 'USA',
+				value: '美国'
+			},
+			{
+				name: 'CHN',
+				value: '中国',
+				checked: 'true'
+			}
+		],
+		radio1: 0,
+		radio2: 0,
+		radio3: 0,
+		radio4: 0,
+		radio5: 0,
+		radio6: 0,
+		checkbox1: [0],
+		checkbox2: [0],
+		checkbox3: [0],
+		checkbox4: [0],
+		checkbox5: [0],
+		checkbox6: [0],
+		sex: [
+			{
+				text: '男',
+				value: 0
+			}, {
+				text: '女',
+				value: 1
+			}, {
+				text: '未知',
+				value: 2
+			},
+		],
+		sex1: [
+			{
+				text: '男',
+				value: 0
+			}, {
+				text: '女',
+				value: 1,
+				disable: true
+			}, {
+				text: '未知',
+				value: 2
+			},
+		],
+		hobby: [
+			{
+				text: '足球',
+				value: 0
+			}, {
+				text: '篮球',
+				value: 1
+			}, {
+				text: '游泳',
+				value: 2
+			},
+		],
+		hobby2: [
+			{
+				text: '足球',
+				value: 0,
+				disable: true
+			}, {
+				text: '篮球',
+				value: 1,
+				disable: true
+			}, {
+				text: '游泳',
+				value: 2
+			},
+		],
+		dataPickerData:[
+			{
+				text: "一年级",
+				value: "1-0",
+				children: [
+					{
+						text: "1.1班",
+						value: "1-1"
+					},
+					{
+						text: "1.2班",
+						value: "1-2"
+					}
+				]
+			},
+			{
+				text: "二年级",
+				value: "2-0"
+			},
+			{
+				text: "三年级",
+				value: "3-0"
+			},
+		],
+		fabPattern:{
+			color: '#7A7E83',
+			backgroundColor: '#fff',
+			selectedColor: '#18AE66',
+			buttonColor: '#18AE66',
+			iconColor: '#fff'
+		},
+		fabContent: [
+			{
+				iconPath: '/static/c1.png',
+				selectedIconPath: '/static/c1_b.png',
+				text: '选项一',
+				active: false
+			},
+			{
+				iconPath: '/static/c2.png',
+				selectedIconPath: '/static/c2_b.png',
+				text: '选项二',
+				active: false
+			},
+			{
+				iconPath: '/static/c3.png',
+				selectedIconPath: '/static/c3_b.png',
+				text: '选项三',
+				active: false
+			}
+		],
+		favData:{
+			checkList: [false, false, false, false, false, false],
+			contentText: {
+				contentDefault: '追番',
+				contentFav: '已追番'
+			}
+		},
+		segmentedData:{
+			items: ['选项卡1', '选项卡2', '选项卡3'],
+			current: 0,
+		},
+		stepsData:{
+			active: 1,
+			list1: [{
+				title: '事件一'
+			}, {
+				title: '事件二'
+			}, {
+				title: '事件三'
+			}, {
+				title: '事件四'
+			}],
+			list2: [{
+				title: '买家下单',
+				desc: '2018-11-11'
+			}, {
+				title: '卖家发货',
+				desc: '2018-11-12'
+			}, {
+				title: '买家签收',
+				desc: '2018-11-13'
+			}, {
+				title: '交易完成',
+				desc: '2018-11-14'
+			}]
+		},
+		tableData:{
+			searchVal: '',
+			tableData: [],
+			// 每页数据量
+			pageSize: 10,
+			// 当前页
+			pageCurrent: 1,
+			// 数据总量
+			total: 0,
+			loading: false
+		},
+		
+	});
+	
+	
+	const calendarChange = (e) =>{
+		console.log('change 返回:', e)
+		// 模拟动态打卡
+		if (data.calendarInfo.selected.length > 5) return
+		data.calendarInfo.selected.push({
+			date: e.fulldate,
+			info: '打卡'
+		})
+	};
+	const setProgress = ()=>{
+		data.progessList = [20, 40, 60, 80]
+	};
+	const clearProgress = ()=>{
+		data.progessList = [0, 0, 0, 0]
+	};
+	
+	const dataPickerDataOnchange = (e)=>{
+		const value = e.detail.value
+	};
+	const dataPickerDataOnnodeclick = ()=>{};
+	
+	const inputValue = ref('');
+	
+	
+	const fabTrigger = (e)=>{
+		console.log(e)
+		data.fabContent[e.index].active = !e.item.active
+		uni.showModal({
+			title: '提示',
+			content: `您${data.fabContent[e.index].active ? '选中了' : '取消了'}${e.item.text}`,
+			success: function(res) {
+				if (res.confirm) {
+					console.log('用户点击确定')
+				} else if (res.cancel) {
+					console.log('用户点击取消')
+				}
+			}
+		})
+	};
+	const fabClick = ()=>{
+		uni.showToast({
+			title: '点击了悬浮按钮',
+			icon: 'none'
+		})
+	};
+	const favClick = (index)=>{
+		data.favData.checkList[index] = !data.favData.checkList[index]
+		console.log(data.favData.checkList[index]);
+		// data.favData.$forceUpdate()
+	}
+	
+	
+	const gotoIndexList = ()=>{
+		uni.navigateTo({
+			url: '/pages/uni-ui/indexList'
+		});
+	};
+	
+	const segmentedDataOnClickItem = e =>{
+		if (data.segmentedData.current !== e.currentIndex) {
+			data.segmentedData.current = e.currentIndex
+		}
+	};
+	
+	const gotoTable = ()=>{
+		uni.navigateTo({
+			url: '/pages/uni-ui/table'
+		});
+	};
+	
+	
+	
+	
+	const {
+		calendarInfo,
+		// 进度
+		progessList,
+		radioItems
+	} = toRefs(data);
+</script>
+
+<style lang="scss" scoped>
+	.container{
+		width: 750rpx;
+		padding: 30rpx 0  190rpx 0;
+	}
+	.progress-box {
+		display: flex;
+		height: 50rpx;
+		margin-bottom: 60rpx;
+	}
+
+	.uni-icon {
+		line-height: 1.5;
+	}
+
+	.progress-cancel {
+		margin-left: 40rpx;
+	}
+
+	.progress-control button {
+		margin-top: 20rpx;
+	}
+	
+	.progress_class{
+		width: 750rpx;
+	}
+	
+	.columnbox{
+		display:flex;
+		flex-flow:column nowrap;
+		& button{
+			width: 600rpx;
+			margin-bottom: 20rpx;
+		}
+		& button[size=mini] {
+			margin-left: auto;
+			margin-right: auto;
+		}
+	}
+	.uni-list,	
+	.rowbox{
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: space-around
+	}
+	.mt-50{
+		margin-top: 50rpx;
+	}
+</style>
