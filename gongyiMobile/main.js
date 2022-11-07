647399bb93db85ffaf4cdabcd4e6a5b81152a31f
Azir <41815835+hotpointchina@users.noreply.github.com>
2022-11-07 08:59:29 +0800

    Add files via upload

--- gongyiMobile/main.js
+++ gongyiMobile/main.js
@@ -13,13 +13,18 @@ const app = new Vue({
 app.$mount()
 // #endif
 
+
+
 // #ifdef VUE3
 import { createSSRApp } from 'vue'
+import * as Pinia from 'pinia';
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
+	app.use(Pinia.createPinia());
   return {
-    app
+    app,
+		Pinia,
   }
 }
 // #endif
